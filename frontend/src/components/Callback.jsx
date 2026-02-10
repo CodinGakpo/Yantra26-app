c4d273f9d63faefdb650df27571dd0db0f8f9b64
Avadhoot <arcsmo19@gmail.com>
2026-02-10 18:51:15 +0530

    feat: added app

--- frontend/src/components/Callback.jsx
+++
@@ -1,9 +0,0 @@
-const Callback = () => {
-  return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <p>OAuth callback - to be implemented</p>
-    </div>
-  );
-};
-
-export default Callback;
--- frontend/src/config/backend.js
+++
@@ -1,5 +0,0 @@
-export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
-if (!BACKEND_BASE_URL) {
-  throw new Error("VITE_BACKEND_URL not defined");
-}
